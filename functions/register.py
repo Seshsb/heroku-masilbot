3f9bd22f0afc72dbd7815e7bed78c4118750e95c
Li Ruslan Ruslanovich <RLi@beeline.uz>
2022-09-22 23:14:10 +0500

    Добавление юзеров

--- functions/register.py
+++ functions/register.py
@@ -4,11 +4,11 @@ from telebot import types
 from db import operations
 
 
-def register(msg, bot):
+def register(message, bot):
     text = 'Отлично, вы успешно зарегистрированы'
-    if msg.text[:5] == '+998':
-        if msg.text[1:].isdigit() and len(msg.text) == 13:
-            phone_number = msg.text
-            operations.create_user(msg.from_user.id, phone_number)
-            return bot.send_message(msg.from_user.id, text)
-        return bot.send_message(msg.from_user.id, 'Введите правильный номер телефона')
+    if message.text[:4] == '+998':
+        if message.text[1:].isdigit() and len(message.text) == 13:
+            phone_number = message.text
+            operations.create_user(message.from_user.id, phone_number)
+            return bot.send_message(message.from_user.id, text)
+        return bot.send_message(message.from_user.id, 'Введите правильный номер телефона')
