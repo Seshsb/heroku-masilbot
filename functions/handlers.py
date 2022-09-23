5ee385d0d673f3bdd5a60131900dcd50f2056bd4
Li Ruslan Ruslanovich <RLi@beeline.uz>
2022-09-23 17:20:33 +0500

    тест

--- functions/handlers.py
+++ functions/handlers.py
@@ -1,11 +1,9 @@
 import datetime
-from db import operations
-from main import bot
 from telebot import types
 from data.config import GET_PHONE_NUMBER, FORMAT_NUMBER_INVALID
 
 
-def reserve_time(message: types.Message):
+def reserve_time(message: types.Message, bot):
     time = message.text
     time_sql = time[:2] + '-' + time[3:5] + ' ' + time[6:]
     bot.send_message(message, GET_PHONE_NUMBER)
@@ -15,7 +13,7 @@ def reserve_time(message: types.Message):
 def get_phone_number(message: types.Message, time, phone_number):
     return message.text[:4] == '+998'
 
-
-@bot.message_handler(func=get_phone_number, content_types=['text', 'contact'])
-def test(message):
-    bot.send_message(message.from_user.id, 'test')
+#
+# @bot.message_handler(func=get_phone_number, content_types=['text', 'contact'])
+# def test(message):
+#     bot.send_message(message.from_user.id, 'test')
--- main.py
+++ main.py
@@ -43,6 +43,11 @@ def text_contacts(message: types.Message):
     bot.send_message(message.from_user.id, str(message))
 
 
+@bot.message_handler(func=get_phone_number, content_types=['text', 'contact'])
+def test(message):
+    bot.send_message(message.from_user.id, 'test')
+
+
 @bot.callback_query_handler(func=lambda call: True)
 def inline_seating_category(call: types.CallbackQuery):
     if call.data == 'tables':
