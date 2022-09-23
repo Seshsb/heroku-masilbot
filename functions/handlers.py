d0416783b37970cbf56e89b00c527e3acae0db8f
Li Ruslan Ruslanovich <RLi@beeline.uz>
2022-09-23 17:26:51 +0500

    тест

--- functions/handlers.py
+++ functions/handlers.py
@@ -6,7 +6,7 @@ from data.config import GET_PHONE_NUMBER
 def reserve_time(message: types.Message, bot):
     time = message.text
     time_sql = time[:2] + '-' + time[3:5] + ' ' + time[6:]
-    bot.send_message(message, 'success')
+    bot.send_message(message.from_user.id, 'success')
     bot.register_next_step_handler(message, get_phone_number, time)
 
 
