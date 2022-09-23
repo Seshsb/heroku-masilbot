ae0c8d07569482e89487cb1f4b91591b08262155
Li Ruslan Ruslanovich <RLi@beeline.uz>
2022-09-23 17:01:55 +0500

    Удаление регистрации и добавление логики

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from flask import Flask, request
 from os.path import join, dirname
 from dotenv import load_dotenv
 from db import operations
-from functions.handlers import reserve_time
+from functions.handlers import reserve_time, get_phone_number
 from keyboards.default import navigation, register
 from data.config import START
 from keyboards.inline.navigations import inline_category
@@ -36,11 +36,35 @@ def start(message: types.Message):
 @bot.message_handler(content_types=['contact', 'text'])
 def text_contacts(message: types.Message):
     if message.text == 'Бронирование':
-        bot.send_message(message.from_user.id, 'Выберите категорию посадочных мест', reply_markup=inline_category())
+        bot.send_message(message.from_user.id, 'Выберите категорию посадочных мест',
+                         reply_markup=inline_category())
 
     # booking(message, bot)
     bot.send_message(message.from_user.id, str(message))
 
+
+@bot.message_handler(func=reserve_time, content_types=['text', 'contact'])
+def test(message):
+    if message.text[:4] == '+998':
+        if message.text[1:].isdigit() and len(message.text) == 13:
+            phone_number = message.text
+            operations.create_user(message.from_user.id, phone_number)
+            return bot.send_message(message.from_user.id, '1')
+        return bot.send_message(message.from_user.id, '0')
+
+    elif message.text[1:].isdigit and message.text[:4] != '+998':
+        return bot.send_message(message.from_user.id, '1')
+
+    elif message.content_type == 'contact':
+        if message.contact.phone_number[:3] == '998' and len(message.contact.phone_number) == 12:
+            phone_number = '+' + message.contact.phone_number
+        elif message.contact.phone_number[0:4] == '+998' and len(message.contact.phone_number) == 12:
+            phone_number = message.contact.phone_number
+        else:
+            return bot.send_message(message.from_user.id, '0')
+
+        operations.create_user(message.from_user.id, phone_number)
+        return bot.send_message(message.from_user.id, '1')
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -48,7 +72,7 @@ def inline_seating_category(call: types.CallbackQuery):
     if call.data == 'tables':
         bot.send_message(call.from_user.id, 'Отправьте дату и время на которое хотите забронировать \n'
                                             'В формате: дд.мм ЧЧ:ММ. В 24 часовом формате времени')
-        bot.register_next_step_handler(call.message, reserve_time, bot, time)
+        bot.register_next_step_handler(call.message, reserve_time, bot)
 
 
 @server.route(f'/{BOT_TOKEN}', methods=['POST'])
