9cdfee7d8aa50b8970b2d0199f6bbfcb9944b7b3
Li Ruslan Ruslanovich <RLi@beeline.uz>
2022-09-26 16:28:55 +0500

    Добавление выбора стола

--- main.py
+++ main.py
@@ -55,15 +55,17 @@ def request_contact(message):
 def phone(message):
     global phone_number
     phone_number = message.text
-    bot.send_message(message.from_user.id, GET_FIRST_NAME)
-    dbworker.set_states(message.from_user.id, config.States.S_BOOKING_FIRSTNAME.value)
+    bot.send_photo(message.from_user.id, open('heroku-masilbot/static/booking/tables.jpeg', 'rb'), GET_TABLEID,
+                   reply_markup=choice_table())
+    dbworker.set_states(message.from_user.id, config.States.S_CHOICE_TABLE_ID.value)
 
 
 @bot.message_handler(func=lambda message: dbworker.get_current_state(message.from_user.id) == config.States.S_BOOKING_FIRSTNAME.value)
 def get_first_name(message):
     global first_name
     first_name = message.text
-    bot.send_photo(message.from_user.id, open('heroku-masilbot/static/booking/tables.jpeg', 'rb'), GET_TABLEID, reply_markup=choice_table())
+    bot.send_photo(message.from_user.id, open('heroku-masilbot/static/booking/tables.jpeg', 'rb'), GET_TABLEID,
+                   reply_markup=choice_table())
     dbworker.set_states(message.from_user.id, config.States.S_CHOICE_TABLE_ID.value)
 
 
