3e67405fae7583c0a21df76a98f1408a6eb5fbcb
Li Ruslan Ruslanovich <RLi@beeline.uz>
2022-09-26 11:37:30 +0500

    доработал бронирование

--- db.py
+++ db.py
@@ -37,7 +37,7 @@ class DataBaseOperations:
                 self.cursor.execute('INSERT INTO users (id, first_name, phone_number) '
                                     'VALUES (%s, %s, %s);', (user_id, name, phone_number))
             else:
-                self.cursor.execute('UPDATE users SET phone_number WHERE id=%s;', (phone_number))
+                self.cursor.execute('UPDATE users SET phone_number=%s WHERE id=%s;', (phone_number, user_id))
             self.cursor.execute('INSERT INTO booking (tbl_id, start_at, user_phone) '
                                 'VALUES (%s, %s, %s);', (table_id, time_at, phone_number))
             self.connection.commit()
