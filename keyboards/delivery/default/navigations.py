ff6bc01882ef5dbd5020baf2b13b146f91db4ff9
Li Ruslan Ruslanovich <RLi@beeline.uz>
2022-10-04 11:24:00 +0500

    Старт доставки

--- keyboards/delivery/default/navigations.py
+++ keyboards/delivery/default/navigations.py
@@ -6,7 +6,11 @@ def food_categoriesRu():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=True)
     markup.add(*[category[0] for category in delivery.get_categories()])
 
+    return markup
+
 
 def dishesRu(cat_id):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=True)
     markup.add(*[dish[0] for dish in delivery.get_dishes(cat_id)])
+
+    return markup
