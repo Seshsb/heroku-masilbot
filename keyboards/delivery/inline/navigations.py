54d95713ee3d43d173a5bcfc593e884cd1932399
Li Ruslan Ruslanovich <RLi@beeline.uz>
2022-10-06 00:20:47 +0500

    Добавил фотографии

--- keyboards/delivery/inline/navigations.py
+++ keyboards/delivery/inline/navigations.py
@@ -5,6 +5,6 @@ q=[food[0] for food in deliveryDB.show_basket(275755142)]
 print(q)
 def inline_order(user_id):
     markup = types.InlineKeyboardMarkup(row_width=1)
-    cancel = [types.InlineKeyboardButton(f'{food[0]}', callback_data=f'{food[0]} delete') for food in deliveryDB.show_basket(user_id)]
+    cancel = [types.InlineKeyboardButton(f'{food[0]}', callback_data=f'{food[0]} delete') for food in deliveryDB.show_basket(int(user_id))]
     markup.add(types.InlineKeyboardButton('Оформить заказ', callback_data='order'), *cancel)
     return markup
