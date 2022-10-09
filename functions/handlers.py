898de0d15c198421beb5e3eec8a38403e038da7a
Li Ruslan Ruslanovich <RLi@beeline.uz>
2022-10-09 19:38:21 +0500

    Тест геокодинг

--- functions/handlers.py
+++ functions/handlers.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 import requests
 from connections import YANDEX_TOKEN
 
@@ -5,16 +7,17 @@ from connections import YANDEX_TOKEN
 def get_address_from_coords(coords):
     #заполняем параметры, которые описывались выже. Впиши в поле apikey свой токен!
     PARAMS = {
-        "apikey":YANDEX_TOKEN,
-        "format":"json",
-        "lang":"ru_RU",
-        "kind":"house",
+        "apikey": YANDEX_TOKEN,
+        "format": "json",
+        "lang": "ru_RU",
+        "kind": "house",
         "geocode": coords
     }
 
     #отправляем запрос по адресу геокодера.
     try:
         r = requests.get(url="https://geocode-maps.yandex.ru/1.x/", params=PARAMS)
+        pprint(r)
         #получаем данные
         json_data = r.json()
         #вытаскиваем из всего пришедшего json именно строку с полным адресом.
