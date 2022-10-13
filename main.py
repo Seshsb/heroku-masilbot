7650465a3d09940f14ab38a0613b4f25acb294d7
Li Ruslan Ruslanovich <RLi@beeline.uz>
2022-10-13 17:03:37 +0500

    Конец заказа

--- main.py
+++ main.py
@@ -437,7 +437,7 @@ def show_order_client(client):
         order_admin += '<b>{0}</b>\n{1} x {2:,} = {3:,}\n\n'.format(good[0], good[2], good[1], good[-1]).replace(',', ' ')
     total_amount = total + int(amount)
     order_admin += '\n\n\n<b>Сумма заказа: {0:,} сум\n' \
-                   'Сумма доставки: {0:,}\n' \
+                   'Сумма доставки: {1:,}\n' \
                    'Итого:</b>\n\n' \
                    'Для связи с оператором @seshsb'.format(total, amount).replace(',', ' ')
     bot.send_message(client, order_admin, parse_mode='html', reply_markup=accepting_order())
