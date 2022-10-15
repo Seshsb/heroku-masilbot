e6b4a13cc9b6e86732f78ad7cded630e2d49a1e3
Li Ruslan Ruslanovich <RLi@beeline.uz>
2022-10-15 16:16:04 +0500

    Доработал бронирование

--- main.py
+++ main.py
@@ -161,9 +161,10 @@ def request_people(message: types.Message):
                     bot.send_message(message.from_user.id, 'Извините, но число людей не может превышать допустимого количества\n'
                                                            f'В эту кабинку разрешается от {min_capacity} до {max_capacity}')
                     bot.send_message(message.from_user.id, BOOKING_REQUEST_CATEGORY, reply_markup=inline_category())
-                    return dbworker.set_states(message.from_user.id, config.States.S_BOOKING_SEATING_CATEGORY.value)
-            bot.send_message(message.from_user.id, GET_PHONE_NUMBER, reply_markup=register.send_contact())
-            dbworker.set_states(message.from_user.id, config.States.S_BOOKING_PHONE_NUMBER.value)
+                    dbworker.set_states(message.from_user.id, config.States.S_BOOKING_SEATING_CATEGORY.value)
+            else:
+                bot.send_message(message.from_user.id, GET_PHONE_NUMBER, reply_markup=register.send_contact())
+                dbworker.set_states(message.from_user.id, config.States.S_BOOKING_PHONE_NUMBER.value)
     except Exception as err:
         bot.send_message(275755142, f'Ошибка юзера {message.from_user.id}:\n'
                                     f'{traceback.format_exc()}')
