a576f54a7301000f631f9b2b7c73ec441719f52d
Li Ruslan Ruslanovich <RLi@beeline.uz>
2022-10-23 16:57:16 +0500

    refac

--- keyboards/booking/default.py
+++ keyboards/booking/default.py
@@ -4,7 +4,7 @@ from data.config import trans
 
 
 def base(lang):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1, one_time_keyboard=True)
     back = types.KeyboardButton(trans['general'][f'BACK_{lang}'])
     main_page = types.KeyboardButton(trans['general'][f'BACK_TO_MAIN_PAGE_{lang}'])
     markup.add(back, main_page)
@@ -16,7 +16,7 @@ def quantity_people(lang):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3, one_time_keyboard=True)
     back = types.KeyboardButton(trans['general'][f'BACK_{lang}'])
     main_page = types.KeyboardButton(trans['general'][f'BACK_TO_MAIN_PAGE_{lang}'])
-    markup.add(*[str(num) for num in range(1, 10)])
+    markup.add(*[str(num) for num in range(0, 10)])
     markup.add(back)
     markup.add(main_page)
 
--- keyboards/general_nav.py
+++ keyboards/general_nav.py
@@ -39,5 +39,5 @@ def send_contact(lang):
 
 
 def error():
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width= 1)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1, one_time_keyboard=True)
     markup.add(types.KeyboardButton('\start'))
