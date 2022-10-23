1e7a97d33d902ce65357e2f78891b33107486d95
Li Ruslan Ruslanovich <RLi@beeline.uz>
2022-10-23 19:36:12 +0500

    refactoring

--- keyboards/booking/default.py
+++ keyboards/booking/default.py
@@ -16,7 +16,8 @@ def quantity_people(lang):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3, one_time_keyboard=True)
     back = types.KeyboardButton(trans['general'][f'BACK_{lang}'])
     main_page = types.KeyboardButton(trans['general'][f'BACK_TO_MAIN_PAGE_{lang}'])
-    markup.add(*[str(num) for num in range(0, 10)])
+    markup.add(*[str(num) for num in range(1, 10)])
+    markup.add('0')
     markup.add(back)
     markup.add(main_page)
 
